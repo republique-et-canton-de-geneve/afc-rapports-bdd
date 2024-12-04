bc71c62f64054b304cc3285363015694378aeeb6
Payraud Thomas (DIN) <thomas.payraud@etat.ge.ch>
2024-12-04 11:03:26 +0000

    AFCPQDEV-804 : Ajouter une arborescence dans le menu

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -1,23 +1,22 @@
 import type CucumberReportProject from '@/models/cucumber-report-project'
 import { FEATURE_SLUGS } from '@/generated/featureFiles'
 const IGNORED_SLUG = ['urbafc-backend-template']
-const CATEGORIE_PAR_DEFAUT = 'AFC'
+const CATEGORIE_PAR_DEFAUT = 'Orphelins'
 
 const TRANSLATIONS: CucumberReportProject[] = [
-  { name: 'Mes documents', slug: 'mesdocuments-backend', categorie: 'Référentiel' },
-  { name: 'Page d\'accueil', slug: 'pageaccueil-backend', categorie: 'Référentiel' },
-  { name: 'RVI', slug: 'rvi-backend', categorie: 'Référentiel' },
-  { name: 'Assuj IPP', slug: 'assuj-ipp', categorie: 'Référentiel/Assuj' },
-  { name: 'Changement periode assuj', slug: 'form-chgt-assuj-backend', categorie: 'Référentiel/Assuj' },
-  { name: 'Mes comptes', slug: 'mescomptes-backend', categorie: 'Perception' },
-  { name: 'Contrôle barème', slug: 'controle-bareme-lr-backend', categorie: 'Perception/IS' },
-  { name: 'Depot LR', slug: 'depotlr-backend', categorie: 'Perception/IS' },
-  { name: 'Gestion IBAN', slug: 'gestiban-backend', categorie: 'Perception' },
-  { name: 'Prod impot PP', slug: 'prodimpotpp-backend', categorie: 'Taxation' },
-  { name: 'Tax Immo', slug: 'tax-immo-backend', categorie: 'Taxation' },
-  { name: 'RabbitMQ Admin', slug: 'rabbitmq-admin-backend', categorie: 'Taxation'},
-  { name: 'Texte dynamique', slug: 'texte-dynamique-backend', categorie: 'Référentiel' },
-  { name: 'E-démarches Autorisations', slug: 'edmautorisations-backend', categorie: 'Plateforme' },
+  { name: 'Mes documents', slug: 'mesdocuments-backend', categorie: '01-R-AFC/e-démarches' },
+  { name: 'Page d\'accueil', slug: 'pageaccueil-backend', categorie: '01-R-AFC/e-démarches' },
+  { name: 'RVI', slug: 'rvi-backend', categorie: '24-RVI' },
+  { name: 'Assuj IPP', slug: 'assuj-ipp', categorie: '06-R-Fisc/Assujettissement' },
+  { name: 'Changement periode assuj', slug: 'form-chgt-assuj-backend', categorie: '06-R-Fisc/Assujettissement' },
+  { name: 'Mes comptes', slug: 'mescomptes-backend', categorie: '09-R-Perception/e-démarches' },
+  { name: 'Contrôle barème', slug: 'controle-bareme-lr-backend', categorie: '12-R-ImpotSource/e-démarches' },
+  { name: 'Depot LR', slug: 'depotlr-backend', categorie: '12-R-ImpotSource/e-démarches' },
+  { name: 'Gestion IBAN', slug: 'gestiban-backend', categorie: '09-R-Perception/e-démarches' },
+  { name: 'Prod impot PP', slug: 'prodimpotpp-backend', categorie: '10-R-TaxPP' },
+  { name: 'Tax Immo', slug: 'tax-immo-backend', categorie: '25-Taximmo' },
+  { name: 'RabbitMQ Admin', slug: 'rabbitmq-admin-backend', categorie: '01-R-AFC'},
+  { name: 'Texte dynamique', slug: 'texte-dynamique-backend', categorie: '01-R-AFC/e-démarches' },
 ]
 
 const CUCUMBER_REPORTS: CucumberReportProject[] = FEATURE_SLUGS
