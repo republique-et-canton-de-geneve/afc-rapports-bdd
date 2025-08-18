d83888fafa32ef25c199b9aff2b6c8e5743e130b
Froud Jean-Pierre <jean-pierre.froud@etat.ge.ch>
2025-08-18 13:46:53 +0200

    chore(build): MAJ des dépendances et nettoyage des plugins inutilisés
    - mise à jour des dépendances et des versions des packages dans `package.json` et `template/package.json`
    - suppression du plugin `vite-plugin-vue-devtools` non utilisé dans `vite.config.ts`
    - ajustement de la commande `build` dans `package.json` pour supprimer `run-s` et simplifier l'exécution

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   publicDir: process.env.FEATURES_DIR ? path.resolve(process.env.FEATURES_DIR) : 'public',
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   build: {
     outDir: process.env.OUT_DIR || 'dist'
