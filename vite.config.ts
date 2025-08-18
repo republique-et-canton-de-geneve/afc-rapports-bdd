7b0eab00b5c6c5a6ea4b0e60679ffb9bcdff0cf8
Jean-Pierre Froud <42773140+froudj@users.noreply.github.com>
2025-08-18 08:28:02 +0200

    docs: add template project and step-by-step guide

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,10 +8,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BASE_PATH ?? '/',
+  publicDir: process.env.FEATURES_DIR ? path.resolve(process.env.FEATURES_DIR) : 'public',
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: process.env.OUT_DIR || 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
